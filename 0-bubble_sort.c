dc308e608caa7f9ae9944fdc19a4efba767e3a8f
voicedhealer <vivien.bernardot@gmail.com>
2025-04-11 16:48:16 +0200

    update de sort.h

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,46 +1,32 @@
-#include <stddef.h>
 #include "sort.h"
 
 /**
- * print_array - Affiche un tableau d'entiers
- * @array: Le tableau à afficher
- * @size: Taille du tableau
+ * bubble_sort - A function that sorts an array of int in
+ *				ascending order (bubble sort)
+ *
+ * @array: The array to sort
+ * @size: Number of elements
  */
-void print_array(const int *array, size_t size) {
-    size_t i;
-    
-    for (i = 0; i < size; i++) {
-        printf("%d", array[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
-}
 
-/**
- * bubble_sort - Trie un tableau d'entiers avec l'algorithme de tri à bulles
- * @array: Tableau à trier
- * @size: Taille du tableau
- */
-void bubble_sort(int *array, size_t size) {
-    size_t i, j;
-    int swapped;
-    
-    if (array == NULL || size < 2)
-        return;
-    
-    for (i = 0; i < size - 1; i++) {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
-                print_array(array, size);
-            }
-        }
-        if (!swapped)
-            break;
-    }
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int tmp;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
