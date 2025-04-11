4b69b2d57a540786fa971fbc49c2e4d71b47f8cf
voicedhealer <vivien.bernardot@gmail.com>
2025-04-11 16:58:22 +0200

    update bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,21 +1,21 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Trie un tableau d'entiers par ordre croissant avec Bubble Sort
- * @array: Tableau à trier
- * @size: Taille du tableau
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int tmp, swapped;
+	size_t i = 0, j = 0;
+	int tmp;
 
 	if (!array || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -24,10 +24,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				print_array(array, size);
-				swapped = 1;
 			}
 		}
-		if (!swapped)
-			break;
 	}
 }
