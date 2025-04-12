1fd4af7b6c9e31790b425b7a6c2b8c968542e0bf
voicedhealer <vivien.bernardot@gmail.com>
2025-04-12 18:44:12 +0200

    update the function insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,12 +1,10 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - A function that sorts a doubly linked list of integers
- *						in ascending order using the insertion sort algorithm
- *
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ *                       using the insertion sort algorithm
  * @list: The list containing the integers
  */
-
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *node = (*list)->next, *next_node;
@@ -16,8 +14,8 @@ void insertion_sort_list(listint_t **list)
 	{
 		current = node;
 		prev = current->prev;
-
 		next_node = node->next;
+
 		while (prev != NULL && current->n < prev->n)
 		{
 			tmp = prev->prev;
